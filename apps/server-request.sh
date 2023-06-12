6f99b2f3295456fa814d31406a18ef03f4079c1c
ZuoYuanP <92987321+ZuoYuanP@users.noreply.github.com>
2023-06-12 20:35:27 +0800

    Update server-request.sh

--- apps/server-request.sh
+++ apps/server-request.sh
@@ -1,5 +1,5 @@
 if [[ -n "$1" && "$1" == "--silent" ]]; then
-    cargo run --bin quiche-server -- --cert /home/ubuntu/repos/quiche/apps/src/bin/cert.crt --key /home/ubuntu/repos/quiche/apps/src/bin/cert.key --listen 0.0.0.0:4433 --root /home/ubuntu/repos/quiche/apps/src/bin/root --disable-hystart --no-grease --dgram-proto none --dgram-count 0 &>/dev/null
+    cargo run --bin quiche-server -- --cert /home/test/repos/quiche/apps/src/bin/cert.crt --key /home/test/repos/quiche/apps/src/bin/cert.key --listen 0.0.0.0:4433 --root /home/test/repos/quiche/apps/src/bin/root --disable-hystart --no-grease --dgram-proto none --dgram-count 0 &>/dev/null
 else
-    RUST_LOG=trace cargo run --bin quiche-server -- --cert /home/ubuntu/repos/quiche/apps/src/bin/cert.crt --key /home/ubuntu/repos/quiche/apps/src/bin/cert.key --listen 0.0.0.0:4433 --root /home/ubuntu/repos/quiche/apps/src/bin/root --disable-hystart --no-grease --dgram-proto none --dgram-count 0
+    RUST_LOG=trace cargo run --bin quiche-server -- --cert /home/test/repos/quiche/apps/src/bin/cert.crt --key /home/test/repos/quiche/apps/src/bin/cert.key --listen 0.0.0.0:4433 --root /home/test/repos/quiche/apps/src/bin/root --disable-hystart --no-grease --dgram-proto none --dgram-count 0
 fi
